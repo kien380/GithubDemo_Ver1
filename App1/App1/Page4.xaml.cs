b692f9f825595fdd0a2c322e145dcafd234d40b7
AnMT <maian1891@gmail.com>
2017-02-06 16:49:20 +0700

    - Update code in IL-13

--- App1/App1/Page4.xaml.cs
+++ App1/App1/Page4.xaml.cs
@@ -15,14 +15,15 @@ namespace App1
             InitializeComponent();
         }
 
-        private void Button_Clicked_Function3(object sender, EventArgs e)
+        private async void Button_Clicked_Function3(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new Page1());
         }
 
         private void Button_Clicked_Function4(object sender, EventArgs e)
         {
-
+            _button3.BackgroundColor = Color.Black;
+            _button3.TextColor = Color.White;
         }
     }
 }
